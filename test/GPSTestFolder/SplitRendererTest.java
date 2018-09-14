129b1bd0cbd00f0d9bd4acaf55c49517a3f9b8f0
alocin98 <nicolas.mueller2@gmail.com>
2018-09-14 14:49:03 +0200

    Added SplitRenderAlgorithmus

--- test/GPSTestFolder/SplitRendererTest.java
+++ test/GPSTestFolder/SplitRendererTest.java
@@ -4,15 +4,22 @@ import GPSTestFolder.GPSTest;
 import Rendering.SplitRenderer;
 import Rendering.Vector2;
 import Time.Time;
+import gpxLib.Trackpoint;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SplitRendererTest extends GPSTest {
 
-    Vector2 a = new Vector2(1,2);
-    Vector2 b = new Vector2(2,3);
+    Vector2 a;
+    Vector2 b;
 
+    @Override
+    protected void init() {
+        super.init();
+        a = new Vector2(0,0);
+        b = new Vector2(10,10);
+    }
 
     @Test
     public void initTest(){
@@ -32,7 +39,20 @@ public class SplitRendererTest extends GPSTest {
     @Test
     public void calculateRenderVectorsTest(){
         init();
-
+        Time firstSplit = new Time(0,1,0);
+        Time secondSplit = new Time(0,2,0);
+        SplitRenderer spltrdr = new SplitRenderer(gpsfile, firstSplit, secondSplit, a,b);
+        spltrdr.calculateRenderVectors();
+        Trackpoint tmp = spltrdr.getStartTrackpoint();
+        double deltaX = 0;
+        double deltaY = 0;
+        while(tmp.getNext() != spltrdr.getEndTrackpoint()){
+            deltaX += tmp.getRenderVector().getX();
+            deltaY += tmp.getRenderVector().getY();
+            tmp = tmp.getNext();
+        }
+        assertEquals(10, deltaX);
+        assertEquals(10, deltaY);
     }
 
 
